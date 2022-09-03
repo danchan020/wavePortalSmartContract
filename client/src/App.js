cb13e8f43688799f240b310e5425b3645d5782ff
Daniel <danchan020@gmail.com>
2022-09-02 17:27:27 -0700

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ export default function App() {
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Wave at Me again and again
         </button>
       </div>
     </div>
