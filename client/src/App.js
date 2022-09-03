7d309e8487fbd982c1a406f7cc5d5c9aa5f5cac6
Daniel <danchan020@gmail.com>
2022-09-02 17:21:53 -0700

    react

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        My name is Daniel. This is my WavePortal where I collect your waves! Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,20 +7,14 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
     
-    mapping(address => uint) public userToWave;
 
     constructor(){
         console.log("making a wave smart contract");
     }
 
-    function updateWalletWaves(uint newWaves) public {
-        userToWave[msg.sender] = newWaves;
-    }
-
     function wave() public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
-        updateWalletWaves();
     }
 
     function getTotalWaves() public view returns (uint256) {
